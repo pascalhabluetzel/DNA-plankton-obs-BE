d55279a135c8cf2e77cec20394a8d0aa23344f65
Pascal Hablützel <68325528+pascalhabluetzel@users.noreply.github.com>
2025-02-22 13:22:20 +0100

    Create blastn.sh

---
+++ scripts/blastn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+QUERY_FILE="/data/gent/vo/001/gvo00125/vsc44164/EMOBON/MAGs_2025/results/20250219/330/all/final.contigs.fa"
+DB_NAME="/data/gent/vo/001/gvo00125/vsc44164/EMOBON/MAGs_2025/data/PR2/pr2_version_5.0.0_SSU_taxo_long.fasta"
+OUTPUT_FILE="/data/gent/vo/001/gvo00125/vsc44164/EMOBON/MAGs_2025/results/20250219/blast_results_all.csv"
+EVALUE="1e-5"
+MAX_HITS=1  # Only report the top hit
+THREADS=32
+
+blastn -query "$QUERY_FILE" -db "$DB_NAME" -outfmt "6 qseqid sseqid length pident" -max_target_seqs "$MAX_HITS" -num_threads "$THREADS" -evalue "$EVALUE" > blast_results.tmp
+
+echo "Query,Best_Hit,Alignment_Length,Percentage_Identity" > "$OUTPUT_FILE"
+awk 'BEGIN {FS="\t"; OFS=","} {print $1, $2, $3, $4}' blast_results.tmp >> "$OUTPUT_FILE"
+rm blast_results.tmp
